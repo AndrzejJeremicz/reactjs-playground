55a45aaf597c6b92a796f89281f6e9192850e49a
Andrzej Jeremicz <eljoaquin27@gmail.com>
2015-09-10 10:38:13 +0200

    the final 0.1.0 version

--- components/AppRoot.jsx
+++ components/AppRoot.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ListStore from '../stores/ListStore';
+import AppDispatcher from '../dispatcher/AppDispatcher';
+
+// Sub components
+import NewItemForm from './NewItemForm';
 
 //Method to retrive state from Stores
 let getListState = () => {
@@ -10,18 +14,44 @@ let getListState = () => {
 
 class AppRoot extends React.Component {
 
+	// Method to setState based upon Store changes
+	_onChange() {
+		this.setState(getListState());
+	}
+
 	constructor() {
 		super();
 		this.state = getListState();
 	}
 
+	// Add change listeners to stores
+	componentDidMount() {
+		ListStore.addChangeListener(this._onChange.bind(this));
+	}
+
+	// Remove change listeners from stores
+	componentWillUnmount() {
+		ListStore.removeChangeListener(this._onChange.bind(this));
+	}
+
+	removeItem(e) {
+
+		let id = e.target.dataset.id;
+
+		AppDispatcher.dispatch({
+			action: 'remove-item',
+			id: id
+		});
+	}
+
 	render() {
 
+		let _this = this;
 		let items = ListStore.getItems();
-
 		let itemHtml = items.map(( listItem ) => {
 			return <li key={ listItem.id }>
-					{ listItem.name }
+					{ listItem.name } 
+					<button onClick={ _this.removeItem } data-id={ listItem.id }>x</button>
 				   </li>;
 		});
 
@@ -29,6 +59,7 @@ class AppRoot extends React.Component {
 				<ul>
 					{ itemHtml }
 				</ul>
+				<NewItemForm/>
 			   </div>;
 	}
 }
